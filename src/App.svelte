b416c76f46c94de047542fbeb2844223e33c0b08
Daniel Bañobre Dopico <bytes4life@proton.me>
2023-03-21 12:50:40 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+
+  let tarefas = []
+
+  function manexadorActualizarTarefas() {
+    const promesaDeResposta = fetch("http://localhost:8000/tarefas")
+    promesaDeResposta.then(reaccionAResposta)
+  }
+
+  function reaccionAResposta(resposta) {
+    const promesaDeDatos = resposta.json()
+    promesaDeDatos.then(reaccionOsDatos)
+  }
+
+  function reaccionOsDatos(datos) {
+    tarefas = datos
+  }
+
+</script>
+
+<main>
+
+  <h1>Tarefas</h1>
+
+  <p>Tes {tarefas.length} tarefas na lista.</p>
+
+  <button on:click={manexadorActualizarTarefas}>Actualizar tarefas</button>
+  <span>&lt;- Click aquí</span>
+  
+  {#each tarefas as item}
+    <li>{item.descripcion}<input type="checkbox" checked={item.completada}/></li>
+  {/each}
+
+</main>
